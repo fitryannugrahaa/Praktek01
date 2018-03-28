5dbf297b5fccdd6c37c392ebd0ea64d0582f07f6
fitryannugrahaa <32404079+fitryannugrahaa@users.noreply.github.com>
2018-03-28 12:32:20 +0800

    Mengisi Nilai Atribut

--- src/praktek01/BurungAksi.java
+++ src/praktek01/BurungAksi.java
@@ -12,6 +12,25 @@ package praktek01;
 public class BurungAksi {
     public static void main(String[] args) {
         Burung Merak = new Burung();
-
+        Merak.Jenis_makanan="biji_bijian";
+        Merak.Warna_bulu="Biru";
+        Merak.Paruh="Pendek_runcing";
+        Merak.Kaki="Panjang_ramping";
+        Merak.Ekor="Pertahanan";
+        
+        Burung pipit = new Burung();
+        
+        pipit.Jenis_makanan="biji_bijian";
+        pipit.Warna_bulu="Coklat";
+        pipit.Paruh="Pendek_runcing";
+        pipit.Kaki="Kuat";
+        pipit.Ekor="Penyeimbang";
+        
+        Burung Elang = new Burung();
+        
+        Elang.Jenis_makanan="daging";
+        Elang.Warna_bulu="Hitam";
+        Elang.Paruh="untuk_mecabit_makanan";
+        Elang.Kaki="Mencengkram_mangsa";
     }
 }
