e23051dbfa47c282ce0dfbbba7df729b9f522013
fitryannugrahaa <32404079+fitryannugrahaa@users.noreply.github.com>
2018-03-28 11:58:01 +0800

    Membuat atribut dan behavior

--- src/praktek01/Burung.java
+++ src/praktek01/Burung.java
@@ -10,5 +10,18 @@ package praktek01;
  * @author user
  */
 public class Burung {
+    String Jenis_makanan;
+    String Warna_bulu;
+    String Paruh;
+    String Kaki;
+    String Ekor;
     
+    void cetakInfo() {
+        System.out.println("Jenis_makanan            :"+Jenis_makanan);
+        System.out.println("Warna_bulu               :"+Warna_bulu);
+        System.out.println("Paruh                    :"+Paruh);
+        System.out.println("Kaki                     :"+Kaki);
+        System.out.println("Ekor                     :"+Ekor);
+        
+    }
 }
